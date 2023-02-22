98d8e6f75f6334b3674e7932a42d43980bb65886
dykim <doyune0707@gmail.com>
2023-02-22 16:43:40 +0900

    [feat] 데이터 모델 생성

---
+++ SwiftUIWeatherSample/Model/Forecast.swift
@@ -0,0 +1,37 @@
+//
+//  Forecast.swift
+//  SwiftUIWeatherSample
+//
+//  Created by 김도윤 on 2023/02/22.
+//
+
+import Foundation
+
+struct Forecast: Identifiable {
+    var id = UUID()
+    
+    let date: String
+    let time: String
+    let icon: String
+    let weather: String
+    let temperature: String
+    let minTemperature: String
+    let maxTemperature: String
+    let forecastedDate: Date
+}
+
+extension Forecast {
+    static var preview: [Forecast] {
+        return (0..<10).map {
+            let dt = Date.now.addingTimeInterval(TimeInterval($0 * 3600 * 24))
+            return Forecast(date: dt.formatted(.dateTime.month().day()),
+                            time: dt.formatted(.dateTime.hour().minute()),
+                            icon: "sun.max.fill",
+                            weather: "맑음",
+                            temperature: Double.randomTemperatureString,
+                            minTemperature: Double.randomTemperatureString,
+                            maxTemperature: Double.randomTemperatureString,
+                            forecastedDate: .now)
+        }
+    }
+}
