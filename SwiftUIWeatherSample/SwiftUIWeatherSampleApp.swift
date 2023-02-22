948049ef7072956041911590c93012dc7273e92d
dykim <doyune0707@gmail.com>
2023-02-22 15:43:41 +0900

    [feat] 프로젝트 생성

---
+++ SwiftUIWeatherSample/SwiftUIWeatherSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherSampleApp.swift
+//  SwiftUIWeatherSample
+//
+//  Created by 김도윤 on 2023/02/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeatherSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
