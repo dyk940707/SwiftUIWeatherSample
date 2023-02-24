d085c49ac29f296270c1adf3d0cff954d42350fb
dykim <doyune0707@gmail.com>
2023-02-24 15:39:49 +0900

    [feat] 위치정보 관련 서비스 추가

---
+++ SwiftUIWeatherSample/Service/WeatherService.swift
@@ -0,0 +1,56 @@
+//
+//  WeatherService.swift
+//  SwiftUIWeatherSample
+//
+//  Created by 김도윤 on 2023/02/24.
+//
+
+import Foundation
+import CoreLocation
+
+class WeatherService: NSObject, ObservableObject {
+    let locationManager: CLLocationManager
+    
+    @Published var currentLocation: String?
+    
+    @Published var currentWeather: CurrentWeather?
+    @Published var forecastList: [Forecast]?
+    
+    @Published var lastError: String?
+    
+    let isPreviewService: Bool
+    
+    init(preview: Bool = false) {
+        isPreviewService = preview
+        
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        
+        super.init()
+        
+        locationManager.delegate = self
+    }
+    
+    static var preview: WeatherService {
+        let service = WeatherService(preview: true)
+        service.currentLocation = "강남역"
+        service.currentWeather = CurrentWeather.preview
+        service.forecastList = Forecast.preview
+        return service
+    }
+    
+    func fetch() {
+        guard !isPreviewService else { return }
+        
+        switch locationManager.authorizationStatus {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .authorizedAlways, .authorizedWhenInUse:
+            locationManager.requestLocation()
+        case .denied, .restricted:
+            lastError = "위치 서비스 사용 권한이 없습니다."
+        default:
+            lastError = "알 수 없는 오류가 발생했습니다."
+        }
+    }
+}
